c1c5838897266afe8e2a0b37a42cf65264e852e2
Nues0913 <106960429+Nues0913@users.noreply.github.com>
2023-04-29 17:34:20 +0800

    upgrade

--- custommod/grade.py
+++ custommod/grade.py
@@ -8,7 +8,7 @@ async def spider(account,password,mode):
 	with open('custommod/dic.json', 'r', encoding='utf-8') as j:
 		menu = json.load(j)
 	modedic = {'所有考試排名':2,'所有科目成績':1,'出勤狀況統計':1,'出勤詳細日期':2}
-	# 先找出登入畫面的壞份子，我們需要對login.aspx進行解析
+	# 先找出登入畫面所要求的參數，我們需要對login.aspx進行解析
 	login_url = 'https://svrsql.tnfsh.tn.edu.tw/SCORESTD/Login.aspx'
 	# 先建表，這些是從login.aspx中payload獲取的，這些是我們登入時所需帶的資訊（submix.x and y使用預設值'0'即可）
 	payload = {
@@ -24,14 +24,16 @@ async def spider(account,password,mode):
 	headers = {
 		'User-Agent':
 		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
-# 建立一個session，session有許多功用，諸如自動處理並共享cookie
+	# 建立一個session，session有許多功用，諸如自動處理並共享cookie
 	async with aiohttp.ClientSession(headers=headers) as session:	# 異步上下文管理器
 		async with await session.get(login_url) as resp:
-			assert resp.status == 200	# http不正常時直接拋出AssertionError
-			# 使用異步aiohttp的額外步驟
+			# http不正常時直接拋出AssertionError
+			assert resp.status == 200	
+			# 使用異步aiohttp需要對resp做read()才能分析html
 			html_login = await resp.read()
+			# 使用BeautifulSoup分析html
 			html_login = BeautifulSoup(html_login, "html.parser")
-			# 壞份子的標籤都是input，使用bs4的功能把它們抓出來塞進payload裡
+			# 登入所需參數的html標籤都是input，使用bs4的查找功能把它們抓出來塞進payload裡
 			view_state = html_login.find('input', {'name': '__VIEWSTATE'})['value']
 			view_state_generator = html_login.find('input', {'name': '__VIEWSTATEGENERATOR'})['value']
 			event_validation = html_login.find('input',{'name': '__EVENTVALIDATION'})['value']
@@ -42,6 +44,7 @@ async def spider(account,password,mode):
 			await session.post(login_url, data=payload, headers=headers)
 			# 釋放記憶體
 			del html_login
+			# 配合前端所撰寫的條件式爬蟲
 			if mode == '所有科目成績' or mode == '所有考試排名':
 				response = await session.get(url='https://svrsql.tnfsh.tn.edu.tw/SCORESTD/' + menu['學期考查成績'])
 				html = await response.read()
